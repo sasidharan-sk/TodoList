5d0d56a2766a64e87ff7cb5035ae30e9ee3172b9
sasidharan <61197696+sasidharan-sk@users.noreply.github.com>
2024-04-09 08:42:20 +0530

    Add files via upload

---
+++ Data/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models;
+
+namespace WebApplication1.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+
+        public DbSet<TodoModel> TodoList { get; set; }
+    }
+}
---
+++ Models/TodoModel.cs
@@ -0,0 +1,13 @@
+﻿// TodoModel.cs
+
+using System;
+
+namespace YourNamespace.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+    }
+}
